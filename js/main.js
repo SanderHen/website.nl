442c5d410d2d17c1a9ee077f765a0a2562929b10
Sander Hendrikse <sh@Sanders-MBP.lan>
2021-01-12 22:01:14 +0100

    CSS

--- js/main.js
+++ js/main.js
@@ -23,9 +23,10 @@ function setup() {
                 if(link.style.animation){
                     link.style.animation = '';
                 } else{
-                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 8 + 0.5}s`;
                 }
             })
+
             burger.classList.toggle('toggle');
         })     
     }

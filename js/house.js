e083e2405fbec6117e951dedbe10e39fbe9a4f6d
Sander Hendrikse <sh@Sanders-MacBook-Pro.local>
2021-02-20 21:23:23 +0100

    Gallary Slider

--- js/house.js
+++ js/house.js
@@ -1,3 +1,5 @@
+//@ts-check
+
 function imageGallery() {
   const carouselSlide = document.querySelector('.carousel-slide');
   const carouselImages = document.querySelectorAll('.carousel-slide img');
@@ -8,17 +10,45 @@ function imageGallery() {
 
   //Counter
   let counter = 1;
-  const size = carouselImages[0].clientWidth;
+  const size = carouselImages[0].clientWidth * 6.02;
 
+  // @ts-ignore
   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
   //Button Listeners
-
   nextBtn.addEventListener('click',()=> {
+    if (counter >= carouselImages.length - 1) return;
+    // @ts-ignore
     carouselSlide.style.transition = "transform 0.4 ease-in-out";
     counter++;
+    // @ts-ignore
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
   });
-}
 
-imageGallery();
+  prevBtn.addEventListener('click',()=> {
+    if (counter <= 0) return;
+    // @ts-ignore
+    carouselSlide.style.transition = "transform 0.4 ease-in-out";
+    counter--;
+    // @ts-ignore
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  });
+
+  carouselSlide.addEventListener('transitionend', () => {
+    if(carouselImages[counter].id === 'lastClone'){
+      // @ts-ignore
+      carouselSlide.style.transition = "none"; 
+      counter = carouselImages.length - 2;
+      // @ts-ignore
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if(carouselImages[counter].id === 'firstClone'){
+      // @ts-ignore
+      carouselSlide.style.transition = "none"; 
+      counter = carouselImages.length - counter;
+      // @ts-ignore
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+  });
+
+}

555544bf94d120d20cbed56b1cb9bc16ee31fab8
SanderHen <69893165+SanderHen@users.noreply.github.com>
2021-09-12 17:04:48 +0200

    CSS and JS

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@ function setup() {
     // Image animation
     const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
 
-    portfolioItems.forEach(portfolioItem => {
-        portfolioItem.addEventListener('mouseover', () => {
-            portfolioItem.childNodes[1].classList.add('img-darken');
-        })
-        portfolioItem.addEventListener('mouseout', () => {
-            portfolioItem.childNodes[1].classList.remove('img-darken');
-        })
-    })
+    if (screen.width < 1025) {
+        portfolioItem.childNodes[1].classList.add('img-darken');
+    }
 
-    
+    else (
+        portfolioItems.forEach(portfolioItem => {
+            portfolioItem.addEventListener('mouseover', () => {
+                portfolioItem.childNodes[1].classList.add('img-darken');
+            })
+            portfolioItem.addEventListener('mouseout', () => {
+                portfolioItem.childNodes[1].classList.remove('img-darken');
+            })
+        })
+    )
 }
